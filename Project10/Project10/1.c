0edf1f8b4cb86c344e378f12560b967b521ffd70
sukyoujeong <77674739+sukyoujeong@users.noreply.github.com>
2021-07-18 16:03:03 +0900

    Add files via upload

---
+++ Project10/Project10/1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+int main()
+{
+	int a;
+
+
+	scanf("%d", &a);
+
+	for (int i = 0; i<9; i++)
+	{
+		printf("%d * %d = %d\n", a, i + 1, a * (i + 1));
+	}
+
+}
